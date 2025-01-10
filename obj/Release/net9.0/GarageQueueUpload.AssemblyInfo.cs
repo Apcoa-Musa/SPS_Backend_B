3f5695912a88ed3de8cc685295ca04129d17e741
Musa Vakilov <mvakilov-ep@apcoa.se>
2025-01-10 14:28:54 +0100

    added gitignore

--- obj/Release/net9.0/GarageQueueUpload.AssemblyInfo.cs
+++ obj/Release/net9.0/GarageQueueUpload.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GarageQueueUpload")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789dc24ac77046abd789fbc604e993b0b07aa89e")]
 [assembly: System.Reflection.AssemblyProductAttribute("GarageQueueUpload")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GarageQueueUpload")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
